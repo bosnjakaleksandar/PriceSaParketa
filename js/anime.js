5c2fc7d6072a19d03cff96ee3ea50c3de37990bb
Aleksandar <bosnjakaleksandar02@gmail.com>
2025-01-23 14:15:07 +0100

    Git uploud

---
+++ js/anime.js
@@ -0,0 +1,23 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const containers = document.querySelectorAll(".js-animation");
+
+containers.forEach((container, index) => {
+  const timeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: container,
+      scrub: true,
+      start: "top 50%",
+      end: "top",
+      markers: true,
+    },
+  });
+
+  const slideLeftElements = container.querySelectorAll(".js-slide-left");
+  const slideRightElements = container.querySelectorAll(".js-slide-right");
+  const slideUpElements = container.querySelectorAll(".js-slide-up");
+  timeline
+    .from(slideLeftElements, { opacity: 0, x: -500, duration: 6 })
+    .from(slideRightElements, { opacity: 0, x: 500, duration: 6 })
+    .from(slideUpElements, { opacity: 0, y: 50, duration: 4, stagger: 1 });
+});
