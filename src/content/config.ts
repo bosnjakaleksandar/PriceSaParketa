6dd9191dddb303962f83ad2aa1822f45e2f98da4
Aleksandar <bosnjakaleksandar02@gmail.com>
2025-11-04 21:13:41 +0100

    fix(CMS): Now you can change slug in admin dashboard

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,23 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-  type: "content",
+  type: 'content',
   schema: z.object({
     title: z.string(),
+    slug: z.string().optional(),
     description: z.string(),
     date: z.date(),
     image: z.string().optional(),
-    tags: z.array(z.string()).default(["post"]),
+    tags: z.array(z.string()).default(['post']),
   }),
 });
 
 const pages = defineCollection({
-  type: "content",
+  type: 'content',
   schema: z.object({
     title: z.string(),
-    language: z.enum(["sr", "en"]),
+    slug: z.string().optional(),
+    language: z.enum(['sr', 'en']),
     hero: z.object({
       title: z.string(),
       description: z.string(),
@@ -29,7 +31,7 @@ const pages = defineCollection({
           title: z.string(),
           description: z.string(),
           image: z.string().optional(),
-          imagePosition: z.enum(["left", "right"]).default("right"),
+          imagePosition: z.enum(['left', 'right']).default('right'),
           links: z
             .array(
               z.object({
