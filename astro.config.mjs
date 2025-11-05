3ed096e9c375a6be407518014721d58545c18608
Aleksandar <bosnjakaleksandar02@gmail.com>
2025-11-05 21:18:09 +0100

    fix(SEO): Changea

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
+  site: 'https://pricesaparketa.netlify.app',
+  output: 'static',
+  integrations: [sitemap()],
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: "",
+          additionalData: '',
         },
       },
     },
